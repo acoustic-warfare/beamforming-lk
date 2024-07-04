c989f36b334a984cdb7902806f2d7cf00a75490b
Janne Schyffert <janne.schyffert@me.com>
2024-07-04 11:46:24 +0200

    Add aw_control_unit class

---
+++ src/aw_control_unit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by janne on 2024-07-04.
+//
+
+#include "aw_control_unit.h"
---
+++ src/aw_control_unit.h
@@ -0,0 +1,14 @@
+//
+// Created by janne on 2024-07-04.
+//
+
+#ifndef BEAMFORMER_AW_CONTROL_UNIT_H
+#define BEAMFORMER_AW_CONTROL_UNIT_H
+
+
+class aw_control_unit {
+
+};
+
+
+#endif //BEAMFORMER_AW_CONTROL_UNIT_H
