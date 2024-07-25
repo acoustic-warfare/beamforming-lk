cbfa055c13b41245be1baa48c31d7df9bcdf543e
Tuva <tuva.bjornberg02@gmail.com>
2024-07-25 11:55:32 +0200

    reorder + doc

--- src/audio/audio_wrapper.cpp
+++ src/audio/audio_wrapper.cpp
@@ -8,50 +8,6 @@
 #include <iostream>
 #include <vector>
 
-constexpr int device = 0;
-
-static int audioCallback(const void *_, void *outputBuffer,
-                         unsigned long framesPerBuffer,
-                         const PaStreamCallbackTimeInfo *timeInfo,
-                         PaStreamCallbackFlags statusFlags,
-                         void *userData) {
-
-    auto self = static_cast<AudioWrapper *>(userData);
-    auto out = static_cast<float *>(outputBuffer);
-
-    Pipeline *pipeline = self->getPipeline();
-    std::vector<float> *audioData = self->getAudioData();
-
-    pipeline->barrier();
-
-    float *in = pipeline->getStreams()->get_signal(0, 0);
-
-    for (unsigned long i = 0; i < framesPerBuffer; ++i) {
-        audioData->push_back(in[i]);
-        out[i] = in[i];
-    }
-
-    if (AUDIO_FILE && audioData->size() >= BUFFER_THRESHOLD) {
-        if (MP3) {
-            self->flushBufferMp3();
-        }
-
-        if (WAV) {
-            self->flushBufferWav();
-        }
-
-        audioData->clear();
-    }
-
-    return paContinue;
-}
-
-static void checkErr(PaError err) {
-    if (err != paNoError) {
-        std::cerr << "\nPortAudio error: " << err << std::endl;
-    }
-}
-
 void AudioWrapper::initAudioFiles() {
     if (MP3) {
         lame_ = lame_init();
@@ -83,32 +39,123 @@ void AudioWrapper::initAudioFiles() {
     }
 }
 
-AudioWrapper::AudioWrapper(Pipeline *pipeline) : AudioWrapper(pipeline, debug_) {}
+void AudioWrapper::flushBufferMP3() {
+    std::vector<float> *audioData = getAudioData();
+
+    if (!audioData->empty()) {
+        const int mp3BufferSize = audioData->size();
+        unsigned char mp3Buffer[mp3BufferSize];
+        int numSamples = audioData->size();
+
+        // Encodes for MP3
+        int bytesWritten = lame_encode_buffer_ieee_float(lame_, audioData->data(), nullptr, numSamples, mp3Buffer, mp3BufferSize);
+        if (bytesWritten < 0) {
+            std::cerr << "Error encoding MP3: " << bytesWritten << std::endl;
+        } else {
+            // Writes to MP3 file
+            fwrite(mp3Buffer, 1, bytesWritten, mp3File_);
+        }
+    }
+}
+
+void AudioWrapper::saveToMP3(const std::string &filename) {
+    flushBufferMP3();
+
+    fclose(mp3File_);
+
+    lame_close(lame_);
+    lame_ = nullptr;
+}
+
+void AudioWrapper::flushBufferWav() {
+    std::vector<float> *audioData = getAudioData();
+
+    if (!audioData->empty()) {
+        // Writes to Wav file
+        sf_write_float(sndfile_, audioData->data(), audioData->size());
+    }
+}
+
+void AudioWrapper::saveToWav(const std::string &filename) {
+    flushBufferWav();
+
+    sf_close(sndfile_);
+    sndfile_ = nullptr;
+}
+
+/**
+ * The callback function portaudio continuously runs to process audio. 
+ * Audio data from the pipeline is placed into audioData buffer to be acceses by file encoders,
+ * as well as an out buffer which plays the audio real time. 
+ * Only when incoming data exists it is processed (barrier).
+ */
+static int audioCallback(const void *_, void *outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo *timeInfo,
+                         PaStreamCallbackFlags statusFlags,
+                         void *userData) {
+
+    auto self = static_cast<AudioWrapper *>(userData);
+    auto out = static_cast<float *>(outputBuffer);
+
+    Pipeline *pipeline = self->getPipeline();
+    std::vector<float> *audioData = self->getAudioData();
+
+    pipeline->barrier();
+
+    float *in = pipeline->getStreams()->get_signal(0, 0);
+
+    for (unsigned long i = 0; i < framesPerBuffer; ++i) {
+        audioData->push_back(in[i]);
+        out[i] = in[i];
+    }
+
+    self->processAudioData();
+
+    return paContinue;
+}
+
+static void checkErr(PaError err) {
+    if (err != paNoError) {
+        std::cerr << "\nPortAudio error: " << err << std::endl;
+    }
+}
+
+/**
+ * For debugging and printing current audio devices
+ */
+static void printDevices() {
+    const int deviceAmt = Pa_GetDeviceCount();
+
+    if (deviceAmt <= 0) {
+        std::cerr << "\nError getting device count: " << deviceAmt << std::endl;
+    }
+
+    std::cout << "\nDeviceAmt: " << deviceAmt << std::endl;
+
+    for (int i = 0; i < deviceAmt; ++i) {
+        const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(i);
+        const std::string deviceName = Pa_GetHostApiInfo(deviceInfo->hostApi)->name;
+        std::cout << "\nDevice nr: " << i << " Name: " << deviceInfo->name << " Api: " << deviceName << std::endl;
+        std::cout << "maxInputChannels: " << deviceInfo->defaultSampleRate << std::endl;
+        std::cout << "maxOutputChannels: " << deviceInfo->maxOutputChannels << std::endl;
+        std::cout << "defaultSampleRate: " << deviceInfo->defaultSampleRate << std::endl;
+    }
+}
+
+AudioWrapper::AudioWrapper(Pipeline *pipeline) : AudioWrapper(pipeline, true) {}
 
 AudioWrapper::AudioWrapper(Pipeline *pipeline, bool debug) : pipeline(pipeline), debug_(debug) {
+    // Initializes port audio
     PaError err = paNoError;
     err = Pa_Initialize();
     checkErr(err);
 
     if (debug) {
-        const int deviceAmt = Pa_GetDeviceCount();
-
-        if (deviceAmt <= 0) {
-            std::cerr << "\nError getting device count: " << deviceAmt << ", error code: " << err << std::endl;
-        }
-
-        std::cout << "\nDeviceAmt: " << deviceAmt << std::endl;
-
-        for (int i = 0; i < deviceAmt; ++i) {
-            const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(i);
-            const std::string deviceName = Pa_GetHostApiInfo(deviceInfo->hostApi)->name;
-            std::cout << "\nDevice nr: " << i << " Name: " << deviceInfo->name << " Api: " << deviceName << std::endl;
-            std::cout << "maxInputChannels: " << deviceInfo->defaultSampleRate << std::endl;
-            std::cout << "maxOutputChannels: " << deviceInfo->maxOutputChannels << std::endl;
-            std::cout << "defaultSampleRate: " << deviceInfo->defaultSampleRate << std::endl;
-        }
+        printDevices();
     }
 
+    // Opens port audio stream
     PaStreamParameters out_param;
     out_param.device = Pa_GetDefaultOutputDevice();
     out_param.channelCount = 1;
@@ -125,54 +172,12 @@ AudioWrapper::AudioWrapper(Pipeline *pipeline, bool debug) : pipeline(pipeline),
                         audioCallback,
                         this);
 
-    if (err != paNoError) {
-        std::cerr << "Unable to open stream, error code " << err << std::endl;
-    }
+    checkErr(err);
 
+    // Initializes audio files
     if (AUDIO_FILE) {
         initAudioFiles();
     }
-}
-
-void AudioWrapper::flushBufferMp3() {
-    std::vector<float> *audioData = getAudioData();
-
-    if (!audioData->empty()) {
-        const int mp3BufferSize = audioData->size();
-        unsigned char mp3Buffer[mp3BufferSize];
-        int numSamples = audioData->size();
-
-        int bytesWritten = lame_encode_buffer_ieee_float(lame_, audioData->data(), nullptr, numSamples, mp3Buffer, mp3BufferSize);
-        if (bytesWritten < 0) {
-            std::cerr << "Error encoding MP3: " << bytesWritten << std::endl;
-        } else {
-            fwrite(mp3Buffer, 1, bytesWritten, mp3File_);
-        }
-    }
-}
-
-void AudioWrapper::saveToMp3(const std::string &filename) {
-    flushBufferMp3();
-
-    fclose(mp3File_);
-
-    lame_close(lame_);
-    lame_ = nullptr;
-}
-
-void AudioWrapper::flushBufferWav() {
-    std::vector<float> *audioData = getAudioData();
-
-    if (!audioData->empty()) {
-        sf_write_float(sndfile_, audioData->data(), audioData->size());
-    }
-}
-
-void AudioWrapper::saveToWav(const std::string &filename) {
-    flushBufferWav();
-
-    sf_close(sndfile_);
-    sndfile_ = nullptr;
 }
 
 Pipeline *AudioWrapper::getPipeline() {
@@ -194,6 +199,20 @@ void AudioWrapper::stop_audio_playback() {
     is_on_ = false;
 }
 
+void AudioWrapper::processAudioData() {
+    if (AUDIO_FILE && audioData.size() >= BUFFER_THRESHOLD) {
+        if (MP3) {
+            flushBufferMP3();
+        }
+
+        if (WAV) {
+            flushBufferWav();
+        }
+
+        audioData.clear();
+    }
+}
+
 AudioWrapper::~AudioWrapper() {
     if (is_on_) {
         this->stop_audio_playback();
@@ -204,7 +223,7 @@ AudioWrapper::~AudioWrapper() {
 
     if (AUDIO_FILE) {
         if (MP3) {
-            saveToMp3("output.mp3");
+            saveToMP3("output.mp3");
             std::cout << "Mp3 Saved" << std::endl;
         }
 
--- src/audio/audio_wrapper.h
+++ src/audio/audio_wrapper.h
@@ -17,45 +17,114 @@
 #include "../config.h"
 #include "../pipeline.h"
 
+/**
+ * @class AudioWrapper
+ * @brief Manages audio streaming and processing, with support for file encoding (and real-time playback)
+ */
 class AudioWrapper {
 private:
+    /**
+     * @brief Constructs an AudioWrapper instance with optional debug mode.
+     * @param pipeline Pointer to a Pipeline object for data streaming.
+     * @param debug Flag indicating if debug information should be printed.
+     */
     AudioWrapper(Pipeline *pipeline, bool debug);
-    std::thread producer_thread_;
+
+    // Flag if audio is on
     std::atomic<bool> is_on_ = false;
-    bool debug_ = true;
+
+    // Flag if additional info about audio devices should be printed
+    bool debug_ = false;
+
+    // A PaStream providing acces to audio hardware
     PaStream *audio_stream_ = nullptr;
 
+    // Pipeline to the FPGA from the PC
     Pipeline *pipeline;
+
+    // Vector of audio data filled from the incomming data
     std::vector<float> audioData;
 
-    void initAudioFiles();
-
-    void saveToMp3(const std::string &filename);
-    void saveToWav(const std::string &filename);
-
+    // Struct handling the Wav file
     SNDFILE *sndfile_ = nullptr;
+
+    // Information about the data in the Wav file
     SF_INFO sfinfo_{};
 
+    // The MP3 file
     FILE *mp3File_;
+
+    // Handles informaton about MP3 file, global context handle
     lame_t lame_ = nullptr;
 
+    /**
+     * @brief Initializes and opens Wav & MP3 files
+     */
+    void initAudioFiles();
+
+    /**
+    * @brief Encodes and writes the audio data to an MP3 file.
+    */
+    void flushBufferMP3();
+
+    /**
+     * @brief Adds remaining data to MP3 file and closes it
+     * @param filename The name the output file will receive
+     */
+    void saveToMP3(const std::string &filename);
+
+    /**
+     * @brief Adds remaining data to Wav file and closes it
+     * @param filename The name the output file will receive
+     */
+    void saveToWav(const std::string &filename);
+
+    /**
+    * @brief Writes the audio data to an Wav file.
+    */
+    void flushBufferWav();
+
 public:
+    /**
+     * @brief Constructs an AudioWrapper instance.
+     * @param pipeline Pointer to a Pipeline object for data streaming.
+     */
     explicit AudioWrapper(Pipeline *pipeline);
 
+    // Delete copy constructor and assignment operators.
     AudioWrapper(const AudioWrapper &) = delete;
     AudioWrapper(AudioWrapper &&) = delete;
     AudioWrapper operator=(const AudioWrapper &) = delete;
     AudioWrapper operator=(AudioWrapper &&) = delete;
 
-    void start_audio_playback();
-    void stop_audio_playback();
-
-    void flushBufferMp3();
-    void flushBufferWav();
-
+    /**
+    * @brief Fetches the pipeline
+    */
     Pipeline *getPipeline();
+
+    /**
+    * @brief Fetches the buffer of incoming audio data
+    */
     std::vector<float> *getAudioData();
 
+    /**
+    * @brief Starts the stream of audio
+    */
+    void start_audio_playback();
+
+    /**
+    * @brief Stops the stream of audio
+    */
+    void stop_audio_playback();
+
+    /**
+    * @brief Flushes audio data and writes to files during callback, depending on config choices
+    */
+    void processAudioData();
+
+    /**
+     * @brief Destructor, cleanups of resources.
+     */
     ~AudioWrapper();
 };
 
