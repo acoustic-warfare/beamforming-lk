9bfd07182495c73d07edcb552578537c3d3d424b
Irreq <irreq@protonmail.com>
2024-08-06 12:51:25 +0200

    Mods

--- src/aw_processing_unit/main.cpp
+++ src/aw_processing_unit/main.cpp
@@ -106,6 +106,21 @@ void setupArgumentParser(argparse::ArgumentParser& program) {
             .implicit_value(true)
             .help("Program output");
 
+    program.add_argument("--fps")
+            .default_value(false)
+            .implicit_value(true)
+            .help("Program FPS");
+
+    program.add_argument("--logo")
+            .default_value(false)
+            .implicit_value(true)
+            .help("Use logo");
+
+    program.add_argument("--debug")
+            .default_value(false)
+            .implicit_value(true)
+            .help("Debug mode");
+
     program.add_argument("--fov")
             .default_value(static_cast<float>(180.0f))
             .scan<'g', float>()
@@ -141,6 +156,9 @@ int main(int argc, char* argv[]) {
     std::vector<int> ports = program.get<std::vector<int>>("--port");
     bool verbose = program.get<bool>("--verbose");
     bool use_camera = (camera.compare("false") != 0);
+    bool use_fps = program.get<bool>("--fps");
+    bool use_logo = program.get<bool>("--logo");
+    bool debug = program.get<bool>("--debug");
 
     if (verbose) {
         // Display the parsed argument values
@@ -313,20 +331,23 @@ int main(int argc, char* argv[]) {
                 bigFrames[i].copyTo(result, mask);
                 bigFrames[i] = result;
             }
+            
+            if (debug) {
+                // Define font face
+                int fontFace = cv::FONT_HERSHEY_SIMPLEX;
 
-            // Define font face
-            int fontFace = cv::FONT_HERSHEY_SIMPLEX;
+                // Define font scale (size)
+                double fontScale = 1;
 
-            // Define font scale (size)
-            double fontScale = 1;
+                // Define font color (here it's white)
+                cv::Scalar color(255, 255, 255);
 
-            // Define font color (here it's white)
-            cv::Scalar color(255, 255, 255);
+                std::stringstream ss;
 
-            std::stringstream ss;
-
-            ss << "Trackers: " << awpus[i]->targets().size();
-            //cv::putText(bigFrames[i], ss.str(), cv::Point(0, 20), fontFace, fontScale, color, 2);
+                ss << "Trackers: " << awpus[i]->targets().size();
+                cv::putText(bigFrames[i], ss.str(), cv::Point(0, 20), fontFace, fontScale, color, 2);
+            }
+            
         }
 
         if (awpu_count == 0) {
@@ -343,74 +364,42 @@ int main(int argc, char* argv[]) {
             videoWriter.write(combinedFrame);
         }
 
-        // Calculate FPS
-        frameCount++;
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end - start;
-        if (elapsed.count() >= 1.0) {
-            fps = frameCount / elapsed.count();
-            frameCount = 0;
-            start = end;
+        if (use_fps) {
+            // Calculate FPS
+            frameCount++;
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = end - start;
+            if (elapsed.count() >= 1.0) {
+                fps = frameCount / elapsed.count();
+                frameCount = 0;
+                start = end;
+            }
+
+            // Display FPS on the frame
+            std::string fpsText = "FPS: " + std::to_string(fps);
+            cv::putText(combinedFrame, fpsText, cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 2);
         }
 
-        // Display FPS on the frame
-        std::string fpsText = "FPS: " + std::to_string(fps);
-        cv::putText(combinedFrame, fpsText, cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 2);
 
-//        // Define the top-left corner where the logo will be placed
-//        cv::Point position(10, 10);// Adjust as needed
-//
-//        // Define the region of interest (ROI) in the background image
-//        cv::Rect roi(position.x, position.y, logo.cols, logo.rows);
-//
-//        // Ensure the ROI is within the bounds of the background image
-//        if (roi.x + roi.width > combinedFrame.cols || roi.y + roi.height > combinedFrame.rows) {
-//            std::cerr << "Error: Logo image exceeds background image bounds" << std::endl;
-//            return -1;
-//        }
-//
-//        // Create a mask for the logo image (if it has an alpha channel)
-//        cv::Mat mask;
-//        if (logo.channels() == 4) {
-//            cv::extractChannel(logo, mask, 3);// Extract alpha channel
-//        } else {
-//            mask = cv::Mat::ones(logo.size(), CV_8UC1) * 255;// Create a full-opacity mask if no alpha channel
-//        }
-//
-//        // Create submatrices for the ROI and the logo
-//        cv::Mat combinedFrameROI = combinedFrame(roi);
-//        cv::Mat logoROI = logo(cv::Rect(0, 0, combinedFrameROI.cols, combinedFrameROI.rows));
-//        cv::Mat maskROI = mask(cv::Rect(0, 0, combinedFrameROI.cols, combinedFrameROI.rows));
-//
-//        // Blend the logo onto the background using the mask
-//        cv::Mat blended;
-//        cv::bitwise_and(combinedFrameROI, combinedFrameROI, blended, maskROI);
-//        cv::bitwise_and(logoROI, logoROI, logoROI, maskROI);
-//        //cv::add(blended, logoROI, combinedFrameROI);
-
-        //cv::Mat insetImage(combinedFrame, cv::Rect(100, 0, logo.cols, logo.rows));
-
-        int x = 840;
-        int y = 0;
-        // Convert logo to grayscale if it has an alpha channel or is in color
-        if (logo.channels() == 4 || logo.channels() == 3) {
-            cv::Mat logoGray;
-            cv::cvtColor(logo, logoGray, cv::COLOR_BGR2GRAY);// Convert to grayscale
-            cv::Mat logoColor;
-            cv::applyColorMap(logoGray, logoColor, cv::COLORMAP_BONE);// Apply the same colormap if needed
-            logoColor.copyTo(combinedFrame(cv::Rect(x, y, logoColor.cols, logoColor.rows)));
-        } else if (logo.channels() == 1) {
-            // Logo is already grayscale
-            cv::Mat logoColor;
-            cv::applyColorMap(logo, logoColor, cv::COLORMAP_BONE);// Apply colormap to logo if needed
-            logoColor.copyTo(combinedFrame(cv::Rect(x, y, logoColor.cols, logoColor.rows)));
-        } else {
-            std::cerr << "Error: Unsupported number of channels in the logo image." << std::endl;
+        if (use_logo) {
+            int x = 840;
+            int y = 0;
+            // Convert logo to grayscale if it has an alpha channel or is in color
+            if (logo.channels() == 4 || logo.channels() == 3) {
+                cv::Mat logoGray;
+                cv::cvtColor(logo, logoGray, cv::COLOR_BGR2GRAY);// Convert to grayscale
+                cv::Mat logoColor;
+                cv::applyColorMap(logoGray, logoColor, cv::COLORMAP_BONE);// Apply the same colormap if needed
+                logoColor.copyTo(combinedFrame(cv::Rect(x, y, logoColor.cols, logoColor.rows)));
+            } else if (logo.channels() == 1) {
+                // Logo is already grayscale
+                cv::Mat logoColor;
+                cv::applyColorMap(logo, logoColor, cv::COLORMAP_BONE);// Apply colormap to logo if needed
+                logoColor.copyTo(combinedFrame(cv::Rect(x, y, logoColor.cols, logoColor.rows)));
+            } else {
+                std::cerr << "Error: Unsupported number of channels in the logo image." << std::endl;
+            }
         }
-
-        //combinedFrame = insetImage;
-
-        //logo.copyTo(combinedFrame(cv::Rect(logo.cols, 0, logo.cols, logo.rows)));
 
         // Display the frame
         cv::imshow(APPLICATION_NAME, combinedFrame);
