d89da873f5732027e96ef7f68f98248cd065c33e
IvarNilsson <ivar.j.nilsson@gmail.com>
2024-08-07 14:42:17 +0200

    bug fix! needed a exta false :) @tuvabjornberg

--- src/aw_control_unit/main.cpp
+++ src/aw_control_unit/main.cpp
@@ -7,6 +7,6 @@
 
 int main() {
     AWControlUnit controlUnit(true);
-    controlUnit.Start({21875}, "10.0.0.1",false, "false",
-                          0, true, true, 100, false, false, 180, false, false, false);
+    controlUnit.Start({21875}, "10.0.0.1", false, "false",
+                      0, true, true, 100, false, false, 180, false, false, false, false);
 }
