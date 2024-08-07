95fca879ca10455f9d6e6b000872d2b054811907
Tuva <tuva.bjornberg02@gmail.com>
2024-08-07 11:07:29 +0200

    swcu main patch

--- src/aw_control_unit/main.cpp
+++ src/aw_control_unit/main.cpp
@@ -6,7 +6,7 @@
 #include "aw_control_unit.h"
 
 int main() {
-    AWControlUnit controlUnit;
+    AWControlUnit controlUnit(true);
     controlUnit.Start({21875}, "10.0.0.1",false, "false",
                           0, true, true, 100, false, false, 180, false, false, false);
 }
